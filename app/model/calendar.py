5a770f9ecb37aa371a66ebf0d205634c02c22775
Kevin Cifuentes <kevins_cifuentes@soy.sena.edu.co>
2024-09-12 12:18:31 -0500

    class Reminder finished

--- app/model/calendar.py
+++ app/model/calendar.py
@@ -5,8 +5,16 @@ from typing import ClassVar
 from app.services.util import generate_unique_id, date_lower_than_today_error, event_not_found_error, \
     reminder_not_found_error, slot_not_available_error
 
+@dataclass
+class Reminder:
+    EMAIL = "email"
+    SYSTEM = "system"
 
-# TODO: Implement Reminder class here
+    date_time: datetime
+    type: str = EMAIL
+
+    def __str__(self) -> str:
+        return f"Reminder on {self.date_time} of type {self.type}"
 
 
 # TODO: Implement Event class here
